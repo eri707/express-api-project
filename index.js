4dd652caddd495ac01e4e3eb5f802babb6d6af2e
eri <eri0.sagittarius@gmail.com>
2021-01-26 13:50:46 -0800

    add post routing code

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+app.use(express.text());
 
+let fruitsArray = ['apple', 'orange', 'grapefruit', 'watermelon'];
 app.get('/', (req, res) => {
-  res.json(['apple', 'orange', 'grapefruit', 'watermelon'])
+    res.json(fruitsArray);
+})
+
+app.post('/', (req, res) => {
+    fruitsArray.push(JSON.parse(req.body));
+    res.json(fruitsArray);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
