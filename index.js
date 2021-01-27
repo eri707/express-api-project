50bd982e32148f974baf51c95b5f0e3ed9703a34
eri <eri0.sagittarius@gmail.com>
2021-01-27 14:16:36 -0800

    add delete routing code

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
-app.use(express.text());
+const { uuid } = require('uuidv4');
+app.use(express.json());
 
 let fruitsArray = ['apple', 'orange', 'grapefruit', 'watermelon'];
+let fruitsObject = fruitsArray.map((ele) => {
+    return {
+        id: uuid(),
+        name: ele,
+        expired: false,
+        origin: null
+    }
+})
 // get fruitsArray
 app.get('/', (req, res) => {
-    res.json(fruitsArray);
+    res.json(fruitsObject);
 });
 // add fruit into the fruitArray
 app.post('/', (req, res) => {
-    fruitsArray.push(JSON.parse(req.body));
-    res.json(fruitsArray);
+    req.body.id = uuid();
+    fruitsObject.push(req.body);
+    res.json(fruitsObject);
 });
 // delete fruit from the fruitArray
 app.delete('/', function (req, res) {
     //console.log(req.query.fruit);
-    if (req.query.fruit){
-        const idx = fruitsArray.findIndex(ele => ele == req.query.fruit);
-        console.log(idx);
-        fruitsArray.splice(idx, 1);
-        res.send(fruitsArray);
+    if (req.query.fruit && !req.query.id){
+        const idx = fruitsObject.findIndex(ele => ele.name == req.query.fruit);
+        fruitsObject.splice(idx, 1);
+        res.send(fruitsObject);
+    }else if (req.query.id || (req.query.fruit && req.query.id)){
+        const idx = fruitsObject.findIndex(ele => ele.id == req.query.id);
+        fruitsObject.splice(idx, 1);
+        res.send(fruitsObject);
     }
 });
 
