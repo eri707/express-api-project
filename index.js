7fb7bc65451d9c6a9fb71d979ade854d3da48372
eri <eri0.sagittarius@gmail.com>
2021-01-26 21:07:29 -0800

    add delete routing code

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ const fs = require('fs');
 app.use(express.text());
 
 let fruitsArray = ['apple', 'orange', 'grapefruit', 'watermelon'];
+// get fruitsArray
 app.get('/', (req, res) => {
     res.json(fruitsArray);
-})
-
+});
+// add fruit into the fruitArray
 app.post('/', (req, res) => {
     fruitsArray.push(JSON.parse(req.body));
     res.json(fruitsArray);
-})
+});
+// delete fruit from the fruitArray
+app.delete('/', function (req, res) {
+    //console.log(req.query.fruit);
+    if (req.query.fruit){
+        const idx = fruitsArray.findIndex(ele => ele == req.query.fruit);
+        console.log(idx);
+        fruitsArray.splice(idx, 1);
+        res.send(fruitsArray);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
